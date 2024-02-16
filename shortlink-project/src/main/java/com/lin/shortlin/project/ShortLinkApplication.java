5d9093b3665b2c7c6ffa3a4ce25206c579b0ff68
CN-Linzhisen <1424791485@qq.com>
2024-02-16 13:59:31 +0800

    first commit

---
+++ shortlink-project/src/main/java/com/lin/shortlin/project/ShortLinkApplication.java
@@ -0,0 +1,18 @@
+package com.lin.shortlin.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lzs
+ * @create 2024/02/15
+ * @description
+ */
+
+@SpringBootApplication
+public class ShortLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkApplication.class, args);
+    }
+}
+
